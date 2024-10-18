43ca4ba1b70a56ae70ded398191284bf8e139252
svitlanadykun <svitlanad@elementor.red>
2024-10-18 12:44:30 +0200

    mark as known

--- tests/playwright/sanity/accordion.test.ts
+++ tests/playwright/sanity/accordion.test.ts
@@ -4,6 +4,7 @@ import WpAdminPage from '../pages/wp-admin-page';
 
 test( 'Accordion', async ( { page, apiRequests }, testInfo ) => {
 	// Arrange.
+	test.info().annotations.push( { type: 'issue', description: 'known-issue' } );
 	const wpAdmin = new WpAdminPage( page, testInfo, apiRequests ),
 		editor = await wpAdmin.openNewPage();
 
@@ -12,6 +13,7 @@ test( 'Accordion', async ( { page, apiRequests }, testInfo ) => {
 
 	// Assert
 	await editor.togglePreviewMode();
+	await page.pause();
 	expect( await editor.getPreviewFrame()
 		.locator( '.elementor-widget-wrap > .elementor-background-overlay' )
 		.screenshot( { type: 'jpeg', quality: 90 } ) )
